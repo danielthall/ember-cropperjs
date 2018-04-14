24dc8941d9e42fd8734879c829f66474d90500b8
ynotdraw <tony.ward@outlook.com>
2018-04-13 20:37:37 -0500

    Update dummy app with changes

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -2,13 +2,24 @@
 import Ember from 'ember';
 
 const {
+  get,
+  set,
   Controller
 } = Ember;
 
 export default Controller.extend({
+  aspectRatio: 1,
+  source: 'sinbad2_800x600.jpg',
+
   actions: {
     crop(cropper, e) {
       console.log('cropper', cropper, 'e', e);
+    },
+    sinbad() {
+      set(this, 'source', 'sinbad2_800x600.jpg');
+    },
+    sinbadd() {
+      set(this, 'source', 'sinbadd.jpg');
     }
   }
 });
